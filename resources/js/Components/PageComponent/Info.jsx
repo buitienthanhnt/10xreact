47d8fdecca52f4d019690c880e953722f837268f
thanhnt <buitienthanhnt@gmail.com>
2025-10-11 18:18:05 +0700

    dev: Updatev format code

--- resources/js/Components/PageComponent/Info.jsx
+++ resources/js/Components/PageComponent/Info.jsx
@@ -34,7 +34,7 @@ export default function Info({ pageId }) {
 
 	const onPressItem = useCallback(async (type) => {
 		if (type === 'link') {
-			await navigator.clipboard.writeText(pageId);
+			// await navigator.clipboard.writeText(pageId);
 			setSavelink(true);
 			return;
 		}
@@ -85,7 +85,7 @@ export default function Info({ pageId }) {
 		<div className="bg-white rounded-md p-4">
 			{savelink && <div role="alert" class="mb-4 relative flex w-full p-3 text-sm text-white bg-green-600 rounded-md">
 				saved for page link.
-				<button class="flex items-center justify-center transition-all w-8 h-8 rounded-md text-white hover:bg-white/10 active:bg-white/10 absolute top-1.5 right-1.5" type="button">
+				<button onClick={()=> setSavelink(false)} class="flex items-center justify-center transition-all w-8 h-8 rounded-md text-white hover:bg-white/10 active:bg-white/10 absolute top-1.5 right-1.5" type="button">
 					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-5 w-5" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12"></path></svg>
 				</button>
 			</div>}
--- storage/doc/html.md
+++ storage/doc/html.md
@@ -1,1 +1,4 @@
 # html i-tag fa icon: https://www.w3schools.com/icons/fontawesome_icons_webapp.asp
+
+# web optimate perfomance: https://developer.mozilla.org/en-US/docs/Web/Performance
+# https://web.dev/articles/custom-metrics?utm_source=devtools#resource-timing-api
